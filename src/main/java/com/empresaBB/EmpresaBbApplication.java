e6da287593068754ebddf26e6591baf185db8f45
4ires <132360305+4ires@users.noreply.github.com>
2024-05-23 14:47:58 -0300

    Empresa BB

---
+++ src/main/java/com/empresaBB/EmpresaBbApplication.java
@@ -0,0 +1,13 @@
+package com.empresaBB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpresaBbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpresaBbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/empresaBB/EmpresaBbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresaBB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpresaBbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
